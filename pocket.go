9cb59a49799f27b942c310daf32a296772e3bdbd
Cheney <me@chenye.org>
2015-09-18 11:57:32 +0800

    add ssl

--- pocket.go
+++ pocket.go
@@ -12,6 +12,8 @@ type Config struct {
 	Listen string `flag:"[0]"`
 	Mongo  string `flag:";def=:27171;usage=connect to mongo"`
 	Db     string `flag:"db;def=pocket"`
+	Key    string
+	Crt    string
 }
 
 func main() {
@@ -21,6 +23,14 @@ func main() {
 	InitMongo(cfg.Mongo, cfg.Db)
 	mux := http.NewServeMux()
 	Handler(mux)
+	if cfg.Key != "" {
+		go func() {
+			err := http.ListenAndServeTLS(":443", cfg.Crt, cfg.Key, mux)
+			if err != nil {
+				logex.Error(err)
+			}
+		}()
+	}
 	if err := http.ListenAndServe(":8011", mux); err != nil {
 		logex.Error(err)
 	}
